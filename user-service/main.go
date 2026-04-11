0e0092007ff7c1591c029cb8cd909996d4d021b2
Ian Livingston <iannl2@illinois.edu>
2026-04-10 23:47:54 -0500

    added server boilerplate to main.go and modified directory structure

--- user-service/main.go
+++ user-service/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"net/http"
+	"time"
+)
 
+const PORT = ":8080"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /users", register)
+	srv := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		slog.Error("Server Error", err)
+	}
 }
