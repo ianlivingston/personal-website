0ceec6e4e8ce641e8f2e39b78aaafe0bc70acea1
Ian Livingston <iannl2@illinois.edu>
2026-04-15 02:09:40 -0500

    implemented readJSON and generateSalt helper functions; also adds go.sum

--- user-service/go.mod
+++ user-service/go.mod
@@ -1,3 +1,3 @@
-module UserService
+module github.com/ianlivingston/personal-website/tree/main/user-service
 
 go 1.26
--- user-service/register.go
+++ user-service/register.go
@@ -1,7 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"crypto/rand"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type user struct {
+	usr string
+	pwd string
+}
 
 func register(w http.ResponseWriter, r *http.Request) {
+	var newUser user
+	if err := readJSON(r.Body, &newUser); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+}
+
+func readJSON[T any](data io.Reader, ptr *T) error {
+	if err := json.NewDecoder(data).Decode(ptr); err != nil {
+		return err
+	}
+	return nil
+}
+
+func generateSalt(lengthInBytes int) ([]byte, error) {
+	salt := make([]byte, lengthInBytes)
+
+	if _, err := rand.Read(salt); err != nil {
+		return salt, err
+	}
+
+	return salt, nil
 }
